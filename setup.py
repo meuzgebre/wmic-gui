2889b6ce1a02fa6cbd34abc08ec41a50c078d7f5
meuzgebre <95613515+meuzgebre@users.noreply.github.com>
2023-10-12 20:44:44 -0700

    Create setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import py2exe
+import sys
+
+# Remove the console option
+sys.argv.append('py2exe')
+
+# Define the GUI application
+setup(
+    options={
+        'py2exe': {
+            'bundle_files': 1,  # Create a single executable file
+            'compressed': True,  # Compress the library archive
+            'dll_excludes': ['w9xpopen.exe']  # Exclude unnecessary DLLs
+        }
+    },
+    windows=[{'script': 'main.py'}]  # Replace 'your_gui_script.py' with your script's name
+)

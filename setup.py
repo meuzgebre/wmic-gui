65848ab111a872b992e4bf435c099a72c6248ef6
meuzgebre <95613515+meuzgebre@users.noreply.github.com>
2023-10-13 15:15:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ import py2exe
 import sys
 
 # Remove the console option
-sys.argv.append('py2exe')
+sys.argv.append("py2exe")
 
 # Define the GUI application
 setup(
     options={
-        'py2exe': {
-            'bundle_files': 1,  # Create a single executable file
-            'compressed': True,  # Compress the library archive
-            'dll_excludes': ['w9xpopen.exe']  # Exclude unnecessary DLLs
+        "py2exe": {
+            "bundle_files": 1,  # Create a single executable file
+            "compressed": True,  # Compress the library archive
+            "dll_excludes": ['w9xpopen.exe']  # Exclude unnecessary DLLs
         }
     },
-    windows=[{'script': 'main.py'}]  # Replace 'your_gui_script.py' with your script's name
+    windows=[{"script": "main.py", "dest_base": "wmic gui"}]
 )
